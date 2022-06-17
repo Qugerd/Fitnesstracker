1978167a9ea62d4a51049d92b9dbace983645d6b
Qugerd <qugerd.main@gmail.com>
2022-06-17 19:53:56 +1000

    )

--- app/src/main/java/com/example/fitnesstracker/Login.kt
+++ app/src/main/java/com/example/fitnesstracker/Login.kt
@@ -2,7 +2,6 @@ package com.example.fitnesstracker
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
@@ -14,10 +13,6 @@ class Login : Fragment(R.layout.activity_login) {
 
         view.findViewById<Toolbar>(R.id.toolbar_login).setNavigationOnClickListener {
             findNavController().navigateUp()
-        }
-
-        view.findViewById<Button>(R.id.btn_login).setOnClickListener {
-            findNavController().navigate(R.id.action_login_to_emptystate)
         }
 
     }
