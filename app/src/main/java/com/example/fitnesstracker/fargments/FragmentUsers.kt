41568923b8213960be19e41a44b42c1ff2839dd3
Qugerd <qugerd.main@gmail.com>
2022-06-18 19:32:29 +1000

    Update FragmentUsers.kt

--- app/src/main/java/com/example/fitnesstracker/fargments/FragmentUsers.kt
+++ app/src/main/java/com/example/fitnesstracker/fargments/FragmentUsers.kt
@@ -5,7 +5,7 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.fitnesstracker.R
 
-class FragmentUsers : Fragment(R.layout.activity_fragment_users) {
+class FragmentUsers : Fragment(R.layout.fragment_users) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
     }
