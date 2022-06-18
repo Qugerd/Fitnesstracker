4c4e4c3cd9727dc63a18fba300ae853140ef893a
Qugerd <qugerd.main@gmail.com>
2022-06-18 19:32:22 +1000

    Update FragmentMy.kt

--- app/src/main/java/com/example/fitnesstracker/fargments/FragmentMy.kt
+++ app/src/main/java/com/example/fitnesstracker/fargments/FragmentMy.kt
@@ -5,7 +5,7 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.fitnesstracker.R
 
-class FragmentMy : Fragment(R.layout.activity_fragment_my) {
+class FragmentMy : Fragment(R.layout.fragment_my) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
     }
