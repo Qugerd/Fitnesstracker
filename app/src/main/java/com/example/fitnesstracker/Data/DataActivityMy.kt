9ceaf004cfa98dd9468bcd4dfb7554fea3835677
Qugerd <qugerd.main@gmail.com>
2022-06-19 00:12:04 +1000

    Create DataActivityMy.kt

---
+++ app/src/main/java/com/example/fitnesstracker/Data/DataActivityMy.kt
@@ -0,0 +1,30 @@
+package com.example.myapplication.Data
+
+import com.example.fitnesstracker.ListItem
+import org.joda.time.DateTime
+
+class DataActivityMy {
+
+    private val datePattern = "dd.MM.YYYY"
+
+//    private val data = mapOf<Int, ListItem>(
+//        1 to ListItem.Date(1, DateTime.now().toString(datePattern)),
+//        2 to ListItem.Card(2 , "100 км", "60 мин", "Бег", DateTime.now().toString(datePattern), ""),
+//        3 to ListItem.Date(1, DateTime.now().toString(datePattern)),
+//        4 to ListItem.Card(2 , "100 км", "60 мин", "Бег", DateTime.now().toString(datePattern), ""),
+//        5 to ListItem.Date(1, DateTime.now().toString(datePattern)),
+//        6 to ListItem.Card(2 , "100 км", "60 мин", "Бег", DateTime.now().toString(datePattern), ""),
+//    )
+
+    private val data = listOf<ListItem>(
+        ListItem.Date(0, DateTime.now().toString(datePattern)),
+        ListItem.Card(1 , "100 км", "60 мин", "Бег", DateTime.now().toString(datePattern), ""),
+        ListItem.Date(2, DateTime.now().toString(datePattern)),
+        ListItem.Card(3 , "150 км", "90 мин", "Бег", DateTime.now().toString(datePattern), "")
+    )
+
+
+    fun getData() = data
+
+    private fun getMaxId() = data.maxOf{it.id}
+}
