8bec0f94f26b7f7d7c6fb49632b23645984f36da
Qugerd <qugerd.main@gmail.com>
2022-06-18 23:59:45 +1000

    Update ProfilePage.kt

--- app/src/main/java/com/example/fitnesstracker/ProfilePage.kt
+++ app/src/main/java/com/example/fitnesstracker/ProfilePage.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 
-class ProfileFragment: Fragment(R.layout.activity_fragment_profile)  {
+class ProfilePage: Fragment(R.layout.activity_fragment_profile)  {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
