ee91fec388ff3b3c98632ae2a9760d74406a0fd9
Qugerd <qugerd.main@gmail.com>
2022-06-19 01:00:16 +1000

    dateFormat

--- app/src/main/java/com/example/fitnesstracker/Data/DataActivityMy.kt
+++ app/src/main/java/com/example/fitnesstracker/Data/DataActivityMy.kt
@@ -5,22 +5,13 @@ import org.joda.time.DateTime
 
 class DataActivityMy {
 
-    private val datePattern = "dd.MM.YYYY"
-
-//    private val data = mapOf<Int, ListItem>(
-//        1 to ListItem.Date(1, DateTime.now().toString(datePattern)),
-//        2 to ListItem.Card(2 , "100 км", "60 мин", "Бег", DateTime.now().toString(datePattern), ""),
-//        3 to ListItem.Date(1, DateTime.now().toString(datePattern)),
-//        4 to ListItem.Card(2 , "100 км", "60 мин", "Бег", DateTime.now().toString(datePattern), ""),
-//        5 to ListItem.Date(1, DateTime.now().toString(datePattern)),
-//        6 to ListItem.Card(2 , "100 км", "60 мин", "Бег", DateTime.now().toString(datePattern), ""),
-//    )
+    private val dateFormat = "dd.MM.YYYY"
 
     private val data = listOf<ListItem>(
-        ListItem.Date(0, DateTime.now().toString(datePattern)),
-        ListItem.Card(1 , "14.32 км", "2 часа 30 мин", "Бег", DateTime.now().toString(datePattern), ""),
-        ListItem.Date(2, DateTime.now().toString(datePattern)),
-        ListItem.Card(3 , "228 км", "6 часов", "качели", DateTime.now().toString(datePattern), "")
+        ListItem.Date(0, DateTime.now().toString(dateFormat)),
+        ListItem.Card(1 , "14.32 км", "2 часа 30 мин", "Бег", DateTime.now().toString(dateFormat), ""),
+        ListItem.Date(2, DateTime.now().toString(dateFormat)),
+        ListItem.Card(3 , "228 км", "6 часов", "качели", DateTime.now().toString(dateFormat), "")
     )
 
 
--- app/src/main/java/com/example/fitnesstracker/Data/DataActivityUsers.kt
+++ app/src/main/java/com/example/fitnesstracker/Data/DataActivityUsers.kt
@@ -6,28 +6,16 @@ import org.joda.time.DateTime
 
 class DataActivityUsers {
 
-    private val datePattern = "dd.MM.YYYY"
-
-//    private val data = mapOf<Int, ListItem>(
-//        7 to ListItem.Date(1, DateTime.now().toString(datePattern)),
-//        8 to ListItem.Card(2 , "100 км", "60 мин", "Бег", DateTime.now().toString(datePattern), "@bibik"),
-//        9 to ListItem.Date(1, DateTime.now().toString(datePattern)),
-//        10 to ListItem.Card(2 , "150 км", "60 мин", "Бег", DateTime.now().toString(datePattern), "@bibik"),
-//        11 to ListItem.Date(1, DateTime.now().toString(datePattern)),
-//        12 to ListItem.Card(2 , "200 км", "60 мин", "Бег", DateTime.now().toString(datePattern), "@bibik"),
-//    )
+    private val dateFormat = "dd.MM.YYYY"
 
     private val data = listOf<ListItem>(
-        ListItem.Date(0, DateTime.now().toString(datePattern)),
-        ListItem.Card(1 , "322 км", "10 мин", "Бег", DateTime.now().toString(datePattern), "@Он"),
-        ListItem.Date(2, DateTime.now().toString(datePattern)),
-        ListItem.Card(3 , "1 км", "3 мин", "серфинг", DateTime.now().toString(datePattern), "@она"),
-        ListItem.Date(4, DateTime.now().toString(datePattern)),
-        ListItem.Card(5 , "12 км", "23 мин", "флекс", DateTime.now().toString(datePattern), "@Путин")
+        ListItem.Date(0, DateTime.now().toString(dateFormat)),
+        ListItem.Card(1 , "322 км", "10 мин", "Бег", DateTime.now().toString(dateFormat), "@Он"),
+        ListItem.Date(2, DateTime.now().toString(dateFormat)),
+        ListItem.Card(3 , "1 км", "3 мин", "серфинг", DateTime.now().toString(dateFormat), "@она"),
+        ListItem.Date(4, DateTime.now().toString(dateFormat)),
+        ListItem.Card(5 , "12 км", "23 мин", "флекс", DateTime.now().toString(dateFormat), "@Путин")
     )
 
     fun getData() = data
-
-    //private fun getMaxId() = data.maxOf{it.id}
-
 }
